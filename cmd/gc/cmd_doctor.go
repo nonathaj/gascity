5ea15dbf23d454493c27b26002d64799dbc1e2ed
julianknutsen <julianknutsen@users.noreply.github.com>
2026-04-16 06:14:39 +0000

    doctor: repair stale implicit import caches

--- cmd/gc/cmd_doctor.go
+++ cmd/gc/cmd_doctor.go
@@ -61,6 +61,7 @@ func doDoctor(fix, verbose bool, stdout, stderr io.Writer) int {
 	d.Register(&doctor.CityStructureCheck{})
 	d.Register(&doctor.CityConfigCheck{})
 	registerV2DeprecationChecks(d)
+	d.Register(&doctor.ImplicitImportCacheCheck{})
 
 	// Load config for deeper checks. If it fails, we still run the core
 	// checks above (which will report the parse error).
