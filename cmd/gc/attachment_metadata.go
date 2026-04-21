ca37f82311800993385981f95a8b94b37f2fdd23
Julian Knutsen <julianknutsen@users.noreply.github.com>
2026-04-21 06:33:30 +0000

    fix: drop unused attachment metadata wrappers

--- cmd/gc/attachment_metadata.go
+++ cmd/gc/attachment_metadata.go
@@ -12,15 +12,3 @@ func collectAttachedBeads(parent beads.Bead, store beads.Store, childQuerier Bea
 func attachmentLabel(b beads.Bead) string {
 	return sling.AttachmentLabel(b)
 }
-
-func isAttachedRoot(b beads.Bead) bool {
-	return sling.IsAttachedRoot(b)
-}
-
-func isWorkflowAttachment(b beads.Bead) bool {
-	return sling.IsWorkflowAttachment(b)
-}
-
-func isMoleculeAttachment(b beads.Bead) bool {
-	return sling.IsMoleculeAttachment(b)
-}
